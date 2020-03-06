913eef0b76a52cfd9f3551debd703d52b433e416
Jorbari <jorbarinaaton1@gmail.com>
2020-03-06 21:14:43 +0100

    comment out notification in setting page

--- src/views/Settings/Settings.js
+++ src/views/Settings/Settings.js
@@ -24,7 +24,7 @@ const Settings = () => {
           md={7}
           xs={12}
         >
-          <Notifications />
+          {/* <Notifications /> */}
         </Grid>
         <Grid
           item
