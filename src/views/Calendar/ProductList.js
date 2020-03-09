5ef3536984d9ea5ba07d16e25b83adf04181fb7a
Jorbari <jorbarinaaton1@gmail.com>
2020-03-10 00:44:51 +0100

    endpoint to get interviewer schedule endpoint to create schedule

--- src/views/Calendar/ProductList.js
+++ src/views/Calendar/ProductList.js
@@ -4,9 +4,34 @@ import React, { Component } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import API from '../../services/interviewer';
+import Modals from '../../helpers/modal';
 
 
 class ProductList extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      show: false,
+      date: ''
+    }
+
+    this.getMySchedule = this.getMySchedule.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.setChosenDate = this.setChosenDate.bind(this);
+    this.createSchedule = this.createSchedule.bind(this);
+    this.getMySchedule();
+  }
+  handleShow(){
+    this.setState({show: true});
+  }
+  handleClose(){
+    this.setState({show: false});
+  }
+  setChosenDate(event) {
+    this.setState({date: event.target.value})
+  }
   render() {
     return(
       <div>
@@ -15,7 +40,10 @@ class ProductList extends Component{
             <h2>My Calendar</h2>
           </div>
           <div>
-            <button className="btn btn-warning _new-schedule" >New Schedule</button>
+            <button
+              className="btn btn-warning _new-schedule"
+              onClick={this.handleShow}
+            >New Schedule</button>
           </div>
         </div>
          <div className="calendar_container" >
@@ -46,6 +74,46 @@ class ProductList extends Component{
           
         </div>
       
+
+
+
+        
+      <Modals
+        onHide={this.handleClose}
+        show={this.state.show}
+        title={`Create Schedule`}
+      >
+        <div>
+          <div className="form-group">
+            <label htmlFor="email">Enter Date:</label>
+            <input
+              className="form-control"
+              onChange={this.setChosenDate}
+              placeholder="Enter First Name"
+              type="date"
+              value={this.state.date}
+            />
+            {
+              this.state.date.length == 0 && 
+              <p className="err_sm_" >
+                Field cannot be left blank
+              </p>
+            }
+          </div>
+          
+          <div className="Edit_user_" >
+             <button
+               className="btn btn-primary ml-auto"
+               disabled={this.state.date.length == 0}
+               onClick={this.createSchedule}
+             >Create Schedule
+          </button>
+          </div>
+         
+        </div>
+      </Modals>
+   
+
       </div>
      
     )
@@ -55,6 +123,32 @@ class ProductList extends Component{
 
   handleDateClick = (arg) => { // bind with an arrow function
     console.log(arg);
+  }
+  createSchedule(){
+    const scheduleDate = {
+      date: this.state.date
+    }
+
+    console.log(scheduleDate);
+    API.get('api/schedule/create', scheduleDate).then(
+      res => {
+        console.log(res);
+        this.handleClose();
+      }
+    )
+    .catch(
+      err => console.log(err)
+    )
+  }
+  getMySchedule(){
+    API.get('api/schedule/show').then(
+      res => {
+        console.log(res.data.data);
+      }
+    )
+    .catch(
+      err => console.log(err)
+    )
   }
 }
 export default ProductList;
