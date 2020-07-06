80ddc936e77000fb09e82938cc254ca4563c8ffe
Jorbari <jorbarinaaton1@gmail.com>
2020-07-07 00:26:49 +0100

    fix resend email invitation by admin

--- src/views/Account/components/AccountProfile/AccountProfile.js
+++ src/views/Account/components/AccountProfile/AccountProfile.js
@@ -12,6 +12,9 @@ import {
   Button,
   TextField
 } from '@material-ui/core';
+import API from '../../../../services/general';
+import Snackbar from '@material-ui/core/Snackbar';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -42,6 +45,25 @@ const AccountProfile = props => {
   const { className, ...rest } = props;
 
   const [interViewerEmail, SetInterviewerEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  
+  // Notifier alert
+  const [notifierMessage, setNotifierMessage] = useState('');
+  const [stackModal, setStackModal] = useState({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center'
+  });
+  const { vertical, horizontal, open } = stackModal;
+
+  const handleClose = () => {
+    setStackModal({ ...stackModal, open: false });
+  };
+
+  const handleClick = (newState) => {
+    setStackModal({ open: true, ...newState });
+  };
+
 
   const validateEmail = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -54,6 +76,31 @@ const AccountProfile = props => {
     }
 }
 
+const resendEmailInvitation = () => {
+  setIsLoading(true);
+  if(interViewerEmail.length > 1) {
+    API.get(`api/sendInvite`, {params: {email: interViewerEmail}})
+    .then(
+      res => {
+        SetInterviewerEmail('');
+        setNotifierMessage(res.data.message);
+      }
+    )
+    .catch(
+      err => {
+        console.log(err);
+        setNotifierMessage('An error occurred, please try again!!!');
+        // console.log(err);
+      }
+    ).finally(
+      () => {
+        setIsLoading(false);
+        handleClick({ vertical: 'top', horizontal: 'center' });
+      }
+    )
+  }
+}
+
   const classes = useStyles();
 
   return (
@@ -61,6 +108,13 @@ const AccountProfile = props => {
       {...rest}
       className={clsx(classes.root, className)}
     >
+       <Snackbar
+         anchorOrigin={{ vertical, horizontal }}
+         key={vertical + horizontal}
+         message={notifierMessage}
+         onClose={handleClose}
+         open={open}
+       />
       
       <CardContent>
         <div className={classes.details}>
@@ -78,6 +132,7 @@ const AccountProfile = props => {
           onChange={(event) => validateEmail(event.target.value)}
           required
           type="email"
+          value={interViewerEmail}
           variant="outlined"
         />
       </CardContent>
@@ -88,8 +143,11 @@ const AccountProfile = props => {
         <Button
           className={interViewerEmail ? `${classes.resendInterview}` : `${classes.uploadButton}`}
           color="primary"
+          disabled={interViewerEmail.length < 1 || isLoading}
+          onClick={() => resendEmailInvitation()}
           variant="text"
         >
+          {isLoading && <CircularProgress />} 
           Resend Invitation
         </Button>
       </CardActions>
